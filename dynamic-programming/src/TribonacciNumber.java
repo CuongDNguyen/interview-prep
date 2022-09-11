071efd757c9643dbc0fbcfcad958d9574e262487
Cuong <cduynguyen95@gmail.com>
2022-09-11 18:54:05 -0400

    tribonacci

--- dynamic-programming/src/TribonacciNumber.java
+++ dynamic-programming/src/TribonacciNumber.java
@@ -21,18 +21,22 @@ public class TribonacciNumber {
     }
 
     static int tribonacciRecursive(int n) {
-        int[] cache = new int[n + 1];
-        Arrays.fill(cache, -1);
-        if (n == 0) {
-            return cache[0] = 0;
-        } else if (n == 1) {
-            return cache[1] = 1;
-        } else if ( n == 2) {
-            return cache[2] = 1;
-        } else if (cache[n] != -1) {
-            return cache[n];
-        } else {
-                return cache[n];
-            }
+        int first=0,second=1,third=1;
+        // here first,second and third are the previous three elements
+        if(n<2)
+            return n;
+        // if n=0 or n=1 then the tribonacci number is n itself
+        // else for loop will be executed
+        for(int i=3;i<=n;i++)
+        {
+            int newElement=first+second+third;
+            first=second;
+            second=third;
+            third=newElement;
         }
+        //If n=2 then the for loop is not executed and the initial value of the
+        //variable third is returned which is 1
+        return third;
+        // returning the last element
+    }
 }
