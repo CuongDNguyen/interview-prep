045bae2fcca283d2575e34661e35a7eea9fc8877
Cuong Nguyen <cduynguyen95@gmail.com>
2022-09-17 19:51:31 -0400

    group anagram

--- practice/src/yahoo/GroupAnagrams.java
+++ practice/src/yahoo/GroupAnagrams.java
@@ -1,6 +1,6 @@
 package yahoo;
 
-import java.util.List;
+import java.util.*;
 
 /*
 Given an array of strings strs,
@@ -31,8 +31,21 @@ HashMap
 if list anagrams is empty,
  */
 public class GroupAnagrams {
-    public List<List<String>> groupAnagrams(String[] strs) {
+    public static void main(String[] args) {
+        String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        System.out.println("result: " + groupAnagrams(strs));
+    }
 
-        return null;
+    static List<List<String>> groupAnagrams(String[] strs) {
+        if (strs == null || strs.length == 0) return new ArrayList<>();
+        Map<String, List<String>> map = new HashMap<>();
+        for (String s : strs) {
+            char[] ca = s.toCharArray();
+            Arrays.sort(ca);
+            String keyStr = String.valueOf(ca);
+            if (!map.containsKey(keyStr)) map.put(keyStr, new ArrayList<>());
+            map.get(keyStr).add(s);
+        }
+        return new ArrayList<>(map.values());
     }
 }
